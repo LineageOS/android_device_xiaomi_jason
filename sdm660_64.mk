b3bce16e875b13d75434ff3c4156785e7ece5ad1
Ananda Kishore <kananda@codeaurora.org>
2017-03-27 20:58:15 +0530

    sdm660_64: remove NPOR sensors from hardware list
    HiFi, Humidity and Ambient Temperature are NPOR for SDM660.
    Remove entries from hardware sensors list.
    
    Change-Id: Ia3b953e5a04c1128f10619636ccb43df4b9b4e55

--- sdm660_64.mk
+++ sdm660_64.mk
@@ -106,10 +106,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
-    frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:system/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
-    frameworks/native/data/etc/android.hardware.sensor.relative_humidity.xml:system/etc/permissions/android.hardware.sensor.relative_humidity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:system/etc/permissions/android.hardware.sensor.hifi_sensors.xml
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml 
 
 # FBE support
 PRODUCT_COPY_FILES += \

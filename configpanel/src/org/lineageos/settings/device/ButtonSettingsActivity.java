c9320dfa3fae4b16f84b852fdfb96f32b728b3a5
Bruno Martins <bgcngm@gmail.com>
2017-10-05 23:08:09 +0100

    jason: ConfigPanel: Re-integrate into Settings
     * Apply the default Oreo theme and inherit the layouts from Google
       for the Settings app, in order to keep UI consistency.
    
     * Get rid of SettingsDrawerActivity as it no longer fits in.
    
     * Add required appcompat and preference support libraries.
    
     * Integrate into our custom Button settings category by defining the
       expected action.
    
    Change-Id: Ic2e9768552751a59e49ec00576b202e8223268e7

--- configpanel/src/org/lineageos/settings/device/ButtonSettingsActivity.java
+++ configpanel/src/org/lineageos/settings/device/ButtonSettingsActivity.java
@@ -18,15 +18,14 @@
 package org.lineageos.settings.device;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-import com.android.settingslib.drawer.SettingsDrawerActivity;
-
-public class ButtonSettingsActivity extends SettingsDrawerActivity {
+public class ButtonSettingsActivity extends PreferenceActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new ButtonSettingsFragment()).commit();
     }
 }

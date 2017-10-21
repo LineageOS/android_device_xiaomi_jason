5f72fa11e71a9b8acbac039dea1ba4f38e6a31ba
dianlujitao <dianlujitao@lineageos.org>
2017-10-21 22:10:12 +0800

    jason: Add initial sepolicy
    Change-Id: Ie3866280ea14152e6186f299e0ca25e5923909e3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
 # Sensors
 USE_SENSOR_MULTI_HAL := true
 

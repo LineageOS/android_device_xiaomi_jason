4b028dc8b4e875c639f294b3cf7320b57625b10a
dianlujitao <dianlujitao@lineageos.org>
2018-02-04 15:40:43 +0800

    jason: Remove thermal hal
     * Not used on sdm660.
    
    Change-Id: Ic58bb76b03db667eac96c882b7dffe5a014c8663

--- device.mk
+++ device.mk
@@ -421,11 +421,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.cmc=false \
     ro.vendor.sdk.sensors.gestures=false
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service

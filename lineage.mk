b553fbe31eb526ee40a4c8b0560e4f9fba6009ca
dianlujitao <dianlujitao@lineageos.org>
2018-02-04 19:37:27 +0800

    jason: Rebrand for lineage
    Change-Id: Id106f0b950208f4ddd348091cc77c45bf93615fd

--- lineage.mk
+++ lineage.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/jason/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_jason
 PRODUCT_BRAND := Xiaomi

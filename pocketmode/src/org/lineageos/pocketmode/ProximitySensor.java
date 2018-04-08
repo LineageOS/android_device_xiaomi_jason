05eee94e6acb175df7a8ca967eebdb6223a053a2
Bruno Martins <bgcngm@gmail.com>
2018-04-08 15:20:57 +0100

    jason: PocketMode: Remove useless cast
    Change-Id: I655b0b80ccafdaae422a2464d2b8409f70be0e8c

--- pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
+++ pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
@@ -40,8 +40,7 @@ public class ProximitySensor implements SensorEventListener {
 
     public ProximitySensor(Context context) {
         mContext = context;
-        mSensorManager = (SensorManager)
-                mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
 

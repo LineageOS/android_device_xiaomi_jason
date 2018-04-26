69135600303a3efce424dcc9066119e0601f260e
Bruno Martins <bgcngm@gmail.com>
2018-04-26 22:42:20 +0100

    jason: liblight: Always apply RGB brightness scaling
     * Prior to this change, attention and battery lights weren't handled
       in the same way as notification lights. Apply RGB brightness scaling
       for all of those and avoid relying on the frameworks.
    
    Change-Id: I59ddf5538164f06569ccc7265de0c454f0a847ad

--- liblight/lights.c
+++ liblight/lights.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2014, 2017 The  Linux Foundation. All rights reserved.
  * Not a contribution
  * Copyright (C) 2015 The CyanogenMod Project
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -303,6 +303,7 @@ set_speaker_light_locked(struct light_device_t* dev,
 {
     int white, blink;
     int onMS, offMS, stepDuration, pauseHi;
+    unsigned int alpha;
     char *duty;
 
     if(!dev) {
@@ -326,6 +327,14 @@ set_speaker_light_locked(struct light_device_t* dev,
 
     white = rgb_to_brightness(state);
     blink = onMS > 0 && offMS > 0;
+
+    // Extract brightness from AARRGGBB
+    alpha = (state->color >> 24) & 0xff;
+
+    // Scale brightness if a brightness has been applied by the user
+    if (alpha != 0xff) {
+        white = (white * alpha) / 0xff;
+    }
 
     // disable all blinking to start
     write_int(WHITE_BLINK_FILE, 0);
@@ -400,35 +409,7 @@ set_light_notifications(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
-
-    unsigned int brightness;
-    unsigned int color;
-    unsigned int rgb[3];
-
     g_notification = *state;
-
-    // If a brightness has been applied by the user
-    brightness = (g_notification.color & 0xFF000000) >> 24;
-    if (brightness > 0 && brightness < 0xFF) {
-
-        // Retrieve each of the RGB colors
-        color = g_notification.color & 0x00FFFFFF;
-        rgb[0] = (color >> 16) & 0xFF;
-        rgb[1] = (color >> 8) & 0xFF;
-        rgb[2] = color & 0xFF;
-
-        // Apply the brightness level
-        if (rgb[0] > 0)
-            rgb[0] = (rgb[0] * brightness) / 0xFF;
-        if (rgb[1] > 0)
-            rgb[1] = (rgb[1] * brightness) / 0xFF;
-        if (rgb[2] > 0)
-            rgb[2] = (rgb[2] * brightness) / 0xFF;
-
-        // Update with the new color
-        g_notification.color = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
-    }
-
     handle_speaker_light_locked(dev);
     pthread_mutex_unlock(&g_lock);
     return 0;

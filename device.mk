f23ec0f906dc50284fb170c0b2e3d8aca99e3ac5
dianlujitao <dianlujitao@lineageos.org>
2018-06-28 19:51:21 +0800

    jason: Update fingerprint stuff
     * Add goodix HIDL to manifest
     * Move config to /data/vendor
     * Switch to 2.1 HAL
    
    Change-Id: Iaed9b19ae347474b34b4244583ca9fbad6e18b44

--- device.mk
+++ device.mk
@@ -259,7 +259,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service
+    android.hardware.biometrics.fingerprint@2.1-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml

c3e1387a21c4f72d63eb2bfaaefcaaddb3d32946
LuK1337 <priv.luk@gmail.com>
2018-08-26 15:10:38 +0200

    jason: Set vendor security patch level using VENDOR_SECURITY_PATCH
    Change-Id: Id7e80418c2437132bad3fa404b8a966d64758a06

--- lineage_jason.mk
+++ lineage_jason.mk
@@ -24,10 +24,6 @@ $(call inherit-product, device/xiaomi/jason/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Vendor security patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2018-07-01
-
 PRODUCT_NAME := lineage_jason
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jason

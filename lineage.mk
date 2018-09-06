8b433a64828cadd9adf1c3750d8124e42adac774
dianlujitao <dianlujitao@lineageos.org>
2018-09-06 11:57:03 +0800

    jason: Update build desc/fingerprint from MIUI10 stable
     * Fix mismatched system/vendor build fingerprint as well.
    
    Change-Id: I610ac263d8305fae636558daa5e87bd41983dc46

--- lineage.mk
+++ lineage.mk
@@ -39,7 +39,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="jason" \
     PRODUCT_NAME="jason" \
-    BUILD_FINGERPRINT="Xiaomi/jason/jason:8.1.0/OPM1.171019.019/8.6.27:user/release-keys" \
-    PRIVATE_BUILD_DESC="jason-user 8.1.0 OPM1.171019.019 8.6.27 release-keys"
+    PRIVATE_BUILD_DESC="jason-user 8.1.0 OPM1.171019.019 V10.0.2.0.OCHCNFH release-keys"
+
+BUILD_FINGERPRINT := "Xiaomi/jason/jason:8.1.0/OPM1.171019.019/V10.0.2.0.OCHCNFH:user/release-keys"
 
 TARGET_VENDOR := Xiaomi

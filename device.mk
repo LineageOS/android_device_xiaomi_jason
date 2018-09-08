254a0f52e08efdb1ff5cba04660c046e9e4baae8
dianlujitao <dianlujitao@lineageos.org>
2018-09-08 12:13:22 +0800

    jason: Copy vanilla audio policy to /vendor/etc
     * This is used by pure AOSP GSI, on Lineage APM first loads the
       full-featured config from /vendor/etc/audio/ instead of /vendor/etc/.
    
    Change-Id: I03d26795d4d089d743a231c8dc16018b6281783b

--- device.mk
+++ device.mk
@@ -138,7 +138,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_vanilla.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \

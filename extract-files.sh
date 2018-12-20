036a3c8219b3abb8a5b7a63cff70a8a5d77d8a94
dianlujitao <dianlujitao@lineageos.org>
2018-12-20 17:07:33 +0800

    jason: Hack camera HAL to workaround the broken HAL3
     * After MIUI 10.2.3.0 HAL update, a brand new IOisAPI::CreatOISGyro is
       introduced. And to be different from the previous
       ISensorAPI::CreatMGyroDevice, it cannot be simply disabled via
       persist.camera.dxo or any other system properties. Since OIS with gyro
       under HAL3 is still broken, hack the blob to skip gyro initialization.
     * ASM instructions:
        - Before:
          LDR R0, [R4,#0x5C]
          CMP R0, #1
          BEQ 0x85CDE
        - After:
          NOP
          NOP
          B 0x85D06
     * Note that the old API's initialization is skipped as well, so we can
       set persist.camera.dxo=1 again.
    
    Change-Id: I6c9670a37fbf8c8fe2053d6e1f907c3ac3b4eee3

--- extract-files.sh
+++ extract-files.sh
@@ -88,3 +88,11 @@ patchelf --remove-needed libminikin.so "$CAMERA_SDM660"
 patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $DEVICE_BLOB_ROOT/vendor/lib/libvideorefiner.so
 
 "$MY_DIR"/setup-makefiles.sh
+
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+CAMERA_HAL="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
+
+sed -i \
+    -e 's/\xe0\x6d\x01\x28\x0b\xd0/\x00\xbf\x00\xbf\x1f\xe0/' \
+    "$CAMERA_HAL"

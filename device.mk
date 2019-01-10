6ce1b90d8d285f4b0980a4e1b013083d74ddb8b9
dianlujitao <dianlujitao@lineageos.org>
2019-01-10 17:51:00 +0800

    jason: Update NFC configuration
     * Import libnfc-brcm.conf from LA.UM.7.2.r1-05800-sdm660.0
     * Remove libnfc-nci.conf, it's not used by NQ NFC stack
    
    Change-Id: Iee0a893f2f63929f0cad2f479146bf2c3a0d7391

--- device.mk
+++ device.mk
@@ -408,7 +408,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \

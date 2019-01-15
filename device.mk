b2801bb0c03374b9f79c4e8bca9bfd00c776edde
Bruno Martins <bgcngm@gmail.com>
2019-01-15 00:24:17 +0000

    jason: Replace KeyDisabler class with Touch HAL
    Change-Id: I7b87ae0ad834ba02a78696afe393d9d4f8920fbd

--- device.mk
+++ device.mk
@@ -515,6 +515,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.jason
+
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service

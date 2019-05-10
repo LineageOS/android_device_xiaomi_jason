d265657a010cacc4b62b7d78f1397280d9aa01a7
dianlujitao <dianlujitao@lineageos.org>
2019-05-10 15:59:19 +0800

    jason: Enable aptX offload
    This reverts commit 920d3438f2ebee0baf21a481155c2fad0360da55.
    
    Change-Id: Idfc6085a8683eaca32b20cd50082777b228e0eaf

--- device.mk
+++ device.mk
@@ -211,6 +211,9 @@ PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0
+
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee

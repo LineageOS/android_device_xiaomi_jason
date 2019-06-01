c32e86455c20b3a833f6011fa31ee8b0cc6f6082
dianlujitao <dianlujitao@lineageos.org>
2019-06-01 14:54:11 +0800

    jason: Implement a script for goodix data migration
    Change-Id: I171bbb194594cbc8ef88f63c5b7afa323d5aed16

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -123,3 +123,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.panel_info.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := move_goodix_data.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/move_goodix_data.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)

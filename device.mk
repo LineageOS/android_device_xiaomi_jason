fccbd0ef0084e99156d57a474d314948ede43222
dianlujitao <dianlujitao@lineageos.org>
2019-06-01 14:54:11 +0800

    jason: Implement a script for goodix data migration
    Change-Id: I171bbb194594cbc8ef88f63c5b7afa323d5aed16

--- device.mk
+++ device.mk
@@ -290,7 +290,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.jason
+    android.hardware.biometrics.fingerprint@2.1-service.jason \
+    move_goodix_data.sh
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml

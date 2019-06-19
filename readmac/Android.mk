9e4c355cb51402c4e236bfbb0eae7021f1f98165
dianlujitao <dianlujitao@lineageos.org>
2019-06-19 15:10:55 +0800

    jason: Refactor and convert readmac to C++
     * Convert to C++
     * Refactor using infra from libbase
     * Convert char arrays to constant expression
    
    Change-Id: I1c84fd5c442e7221da9c753e1119cc47628af2c0

--- readmac/Android.mk
+++ readmac/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE           := readmac
 LOCAL_MODULE_TAGS      := optional
 LOCAL_VENDOR_MODULE    := true
-LOCAL_SRC_FILES        := xiaomi_readmac.c
-LOCAL_CFLAGS           += -Wall -Werror
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SRC_FILES        := xiaomi_readmac.cpp
+LOCAL_HEADER_LIBRARIES := libcutils_headers
+LOCAL_SHARED_LIBRARIES := libbase
 include $(BUILD_EXECUTABLE)

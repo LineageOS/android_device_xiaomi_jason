21a452a4b4820f0c59f833c53229ee67b93b76bf
dianlujitao <dianlujitao@gmail.com>
2019-07-14 16:27:16 +0800

    jason: Remove msadp symlink
     * This is for debugging purpose and should not be loaded by ueventd in
       production builds.
    
    Change-Id: I39c893149b678bd9398feb721bf4b50f75775dde

--- Android.mk
+++ Android.mk
@@ -77,15 +77,6 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
-MSADP_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/msadp
-$(MSADP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "MSADP link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /dev/block/bootdevice/by-name/msadp $(dir $@)
-
-ALL_DEFAULT_INSTALLED_MODULES += $(MSADP_SYMLINKS)
-
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM ADSP folder structure: $@"

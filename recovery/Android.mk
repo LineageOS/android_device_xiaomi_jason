52c38724aa8bd4897533d618f395fd963441507d
Han Wang <416810799@qq.com>
2019-09-05 07:07:51 +0200

    jason: Remove LOCAL_MODULE_TAGS := eng usage
    Change-Id: I83aa494618d9c4809d3219aa3785e56b48ef8dfb

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -12,5 +12,4 @@ LOCAL_C_INCLUDES := \
 	bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_jason
-LOCAL_MODULE_TAGS := eng
 include $(BUILD_STATIC_LIBRARY)

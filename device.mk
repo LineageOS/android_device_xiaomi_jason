d6c0e680ccc64ee508af87844f648c7e4a8ce63c
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 20:02:28 +0800

    jason: Update audio config for 10
    Change-Id: I28b9e68f4e317b8faabc5d117026b7e9d2cfd982

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml

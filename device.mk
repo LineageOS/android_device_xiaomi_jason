60fa71437ce2a53b81ac8ff0d2bf1e95209afd80
dianlujitao <dianlujitao@lineageos.org>
2020-02-15 17:02:08 +0800

    jason: Remove fingerprint sensor keylayout
    Change-Id: Ifc209b8f3129776f626743deaa49c776eb94b6d9

--- device.mk
+++ device.mk
@@ -102,9 +102,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \

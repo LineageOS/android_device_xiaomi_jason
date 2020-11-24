ea5bdafc3ff64c3abe220d239eb926fdf44d0963
dianlujitao <dianlujitao@lineageos.org>
2020-11-24 15:23:26 +0800

    jason: Add pocketmode to doze whitelist
    Change-Id: I4b9f2267cfd6fe95d08ea0eb6f7415ba5d81c1b1

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -10,6 +10,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
+LOCAL_REQUIRED_MODULES := pocketmode_whitelist.xml
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
@@ -25,4 +26,11 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+LOCAL_MODULE := pocketmode_whitelist.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/sysconfig
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)

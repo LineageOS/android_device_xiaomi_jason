9f6ad56f42e1c892316212a98a213d2c190ff11c
dianlujitao <dianlujitao@gmail.com>
2021-02-18 18:19:48 +0800

    jason: camera: Remove LOCAL_COPY_HEADERS usages
    Change-Id: Ic4d18c781632740f0f07b46b3936cead8fb10982

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -36,9 +36,6 @@ ifneq (,$(filter msm8996 sdm660 msm8998 apq8098_latv,$(TARGET_BOARD_PLATFORM)))
 endif
 
 LOCAL_CFLAGS += -D_ANDROID_ -DQCAMERA_REDEFINE_LOG
-LOCAL_COPY_HEADERS_TO := mm-camera-interface
-LOCAL_COPY_HEADERS += ../common/cam_intf.h
-LOCAL_COPY_HEADERS += ../common/cam_types.h
 LOCAL_CFLAGS  += -DFDLEAK_FLAG
 LOCAL_CFLAGS  += -DMEMLEAK_FLAG
 LOCAL_LDFLAGS += -Wl,--wrap=open -Wl,--wrap=close -Wl,--wrap=socket -Wl,--wrap=pipe -Wl,--wrap=mmap -Wl,--wrap=__open_2

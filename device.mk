a8e81a7ee1c30987e50485941e67498fe931a967
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-20 11:13:50 +0100

    jason: Decommonize IR
    Change-Id: If5d83ef3dc0169700fcd738f6a50cb38b104c30a

--- device.mk
+++ device.mk
@@ -84,6 +84,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     ConfigPanel
 
+# Consumerir
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.ad=1 \

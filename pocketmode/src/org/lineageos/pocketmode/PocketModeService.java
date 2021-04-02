db52694a760d3e2d1355890e78d8391f21d43104
dianlujitao <dianlujitao@lineageos.org>
2021-04-02 22:18:47 +0800

    jason: Nuke ConfigPanel
    It's as simple as a toggle for pocket mode, let's abuse the proximity
    check on wake toggle and kill the app.
    
    Change-Id: Ic510c04162365b5eade6e88e0eee5ca4a5ef40a6

--- pocketmode/src/org/lineageos/pocketmode/PocketModeService.java
+++ pocketmode/src/org/lineageos/pocketmode/PocketModeService.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016 The CyanogenMod Project
- *               2017 The LineageOS Project
+ *               2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,30 +22,27 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.UserHandle;
 import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.List;
+import lineageos.providers.LineageSettings;
 
 public class PocketModeService extends Service {
     private static final String TAG = "PocketModeService";
     private static final boolean DEBUG = false;
 
-    private static final String CUST_INTENT = "org.lineageos.settings.device.CUST_UPDATE";
-    private static final String CUST_INTENT_EXTRA = "pocketmode_service";
-
-    private static List<BroadcastReceiver> receivers = new ArrayList<BroadcastReceiver>();
-
+    private SettingsObserver mSettingsObserver;
     private ProximitySensor mProximitySensor;
 
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
         mProximitySensor = new ProximitySensor(this);
-
-        IntentFilter custFilter = new IntentFilter(CUST_INTENT);
-        registerReceiver(mUpdateReceiver, custFilter);
+        mSettingsObserver = new SettingsObserver(new Handler());
+        mSettingsObserver.register();
     }
 
     @Override
@@ -58,10 +55,8 @@ public class PocketModeService extends Service {
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
         super.onDestroy();
-        if (receivers.contains(mScreenStateReceiver)) {
-            this.unregisterReceiver(mScreenStateReceiver);
-        }
-        this.unregisterReceiver(mUpdateReceiver);
+        unregisterReceiver(mScreenStateReceiver);
+        mSettingsObserver.unregister();
         mProximitySensor.disable();
     }
 
@@ -91,19 +86,42 @@ public class PocketModeService extends Service {
         }
     };
 
-    private BroadcastReceiver mUpdateReceiver = new BroadcastReceiver() {
+    private final class SettingsObserver extends ContentObserver {
+        private SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void register() {
+            getContentResolver().registerContentObserver(LineageSettings.System.getUriFor(
+                    LineageSettings.System.PROXIMITY_ON_WAKE), false, this);
+
+            update();
+        }
+
+        public void unregister() {
+            getContentResolver().unregisterContentObserver(this);
+        }
+
         @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getBooleanExtra(CUST_INTENT_EXTRA, false)) {
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        private void update() {
+            boolean defaultProximity = getResources().getBoolean(
+                    org.lineageos.platform.internal.R.bool.config_proximityCheckOnWakeEnabledByDefault);
+            boolean proximityWakeCheckEnabled = LineageSettings.System.getIntForUser(
+                    getContentResolver(), LineageSettings.System.PROXIMITY_ON_WAKE, defaultProximity
+                    ? 1 : 0, UserHandle.USER_CURRENT) == 1;
+
+            if (proximityWakeCheckEnabled) {
                 IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
                 screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
                 registerReceiver(mScreenStateReceiver, screenStateFilter);
-                receivers.add(mScreenStateReceiver);
-            } else if (receivers.contains(mScreenStateReceiver)) {
+            } else {
                 unregisterReceiver(mScreenStateReceiver);
-                receivers.remove(mScreenStateReceiver);
                 mProximitySensor.disable();
             }
         }
-    };
+    }
 }
